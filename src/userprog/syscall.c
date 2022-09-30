96a4bd7b4226e940369e6c1a7f3eb916de2a89f3
Kristie Huang <kristie.huang@berkeley.edu>
2022-09-30 23:32:19 +0000

    print exit code even if non-user-requested exit fault

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -26,8 +26,9 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
 
   int syscall_num = args[0];
   if (syscall_num == SYS_EXIT) { /** PROCESS CONTROL SYSCALLS **/
+    if ((sizeof args != 2) || (args[1] == NULL))
+      process_exit();
     f->eax = args[1];
-    printf("%s: exit(%d)\n", thread_current()->pcb->process_name, args[1]);
     process_exit();
   } else if (syscall_num == SYS_PRACTICE) {
     // todo: error check
@@ -49,7 +50,6 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
     }
     return;
   } else { // syscall DNE
-    f->eax = -1;
     return;
   }
 }
