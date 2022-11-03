4d6f665b68e2493fad01759f395f05f608b95729
Diraj Thajali <diraj.thajali@berkeley.edu>
2022-11-03 03:16:51 +0000

    initialize wait_ticks to zero on thread init

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -435,6 +435,7 @@ static void init_thread(struct thread* t, const char* name, int priority) {
   t->effective_priority = priority;
   t->pcb = NULL;
   t->magic = THREAD_MAGIC;
+  t->wait_ticks = 0;
 
   old_level = intr_disable();
   list_push_back(&all_list, &t->allelem);
